ef7319dbc3c2d929e10b891ab1c59e5feffd5668
Your Name <email@example.com>
2018-11-26 23:31:01 +0800

    把导航栏做成公共的js文件减少每个页面编写头部的复杂程度,而且方便修改

---
+++ cf/js/header.js
@@ -0,0 +1,92 @@
+document.writeln("<!DOCTYPE html>");
+document.writeln("<html>");
+document.writeln("<head>");
+document.writeln("<meta charset=\"utf-8\">");
+document.writeln("<title>老年娱乐队</title>");
+document.writeln("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, maximum-scale=1,\
+user-scalable=no\">");
+document.writeln("</head>");
+document.writeln("<body>");
+document.writeln("<nav class=\"navbar navbar-default\">");
+document.writeln("<div class=\"container-fluid\">");
+document.writeln("<div class=\"navbar-header\">");
+document.writeln("<button type=\"button\" class=\"navbar-toggle collapsed\" data-toggle=\"collapse\" data-target=\"#bs-example-navbar-collapse-1\" aria-expanded=\"false\">")
+document.writeln("<span class=\"sr-only\">Toggle navigation</span>");
+document.writeln("<span class=\"icon-bar\"></span>");
+document.writeln("<span class=\"icon-bar\"></span>");
+document.writeln("<span class=\"icon-bar\"></span>");
+document.writeln("</button>");
+document.writeln("<a class=\"navbar-brand\" href=\"#\">老年娱乐队</a>");
+document.writeln("</div>");
+document.writeln("<div class=\"collapse navbar-collapse\" id=\"bs-example-navbar-collapse-1\">");
+document.writeln("<ul class=\"nav navbar-nav\">");
+document.writeln("<li><a href=\"cf.html\">首页<span class=\"sr-only\">(current)</span></a></li>");
+document.writeln("<li class=\"dropdown\">");
+document.writeln("<a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\" role=\"button\" aria-haspopup=\"true\" aria-expanded=\"false\">\
+五图狙击卡点 <span class=\"caret\"></span></a>");
+document.writeln("<ul class=\"dropdown-menu\">");
+document.writeln("  <li><a href=\"cf.html\" title=\"卫星基地卡点\" target=\"_blank\">卫星基地卡点</a></li>");
+document.writeln("  <li role=\"separator\" class=\"divider\"></li>");
+document.writeln("  <li><a href=\"tip2.html\" target=\"_blank\">新黑色城镇</a></li>");
+document.writeln("  <li role=\"separator\" class=\"divider\"></li>");
+document.writeln("  <li><a href=\"tip6.html\" target=\"_blank\">异域小镇</a></li>");
+document.writeln("  <li role=\"separator\" class=\"divider\"></li>");
+document.writeln("  <li><a href=\"tip10.html\" target=\"_blank\">潜艇</a></li>");
+document.writeln("  <li role=\"separator\" class=\"divider\"></li>");
+document.writeln("</ul>");
+document.writeln("</li>");
+document.writeln("<li class=\"dropdown\">");
+document.writeln(" <a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\" role=\"button\" aria-haspopup=\"true\" aria-expanded=\"false\">\
+五个位置的主要负责什么 <span class=\"caret\"></span></a>");
+document.writeln("<ul class=\"dropdown-menu\">");
+document.writeln("  <li><a href=\"sniper.html\" target=\"_blank\">狙击 </a></li>");
+document.writeln("  <li><a href=\"shockworker.html\" target=\"_blank\">突破</a></li>");
+document.writeln("  <li><a href=\"reardefence.html\" target=\"_blank\">反侵(断后)</a></li>");
+document.writeln("  <li role=\"separator\" class=\"divider\"></li>");
+document.writeln("  <li><a href=\"helpdefense.html\" target=\"_blank\">协防</a></li>");
+document.writeln("  <li role=\"separator\" class=\"divider\"></li>");
+document.writeln("  <li><a href=\"migration.html\" target=\"_blank\"> 游走</a></li>");
+document.writeln("</ul>");
+document.writeln("<li class=\"dropdown\">");
+document.writeln("<a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\" role=\"button\" aria-haspopup=\"true\" aria-expanded=\"false\">\
+冲锋战术 <span class=\"caret\"></span></a>");
+document.writeln("<ul class=\"dropdown-menu\">");
+document.writeln("  <li><a href=\"#\">卫星基地</a></li>");
+document.writeln("  <li role=\"separator\" class=\"divider\"></li>");
+document.writeln("  <li><a href=\"tip5.html\" target=\"_blank\">A小道<span class=\"label label-danger\">\
+卫星基地</span></a></li>");
+document.writeln("  <li><a href=\"#\">中路 <span class=\"label label-danger\"> 卫星基地</span></a></li>");
+document.writeln("  <li><a href=\"#\">b通 <span class=\"label label-danger\"> 卫星基地</span></a></li>");
+document.writeln("  <li role=\"separator\" class=\"divider\"></li>");
+document.writeln("  <li><a href=\"#\">异域小镇</a></li>");
+document.writeln("  <li role=\"separator\" class=\"divider\"></li>");
+document.writeln("  <li><a href=\"#\">无</a></li>");
+document.writeln("</ul>");
+document.writeln("</li>");
+document.writeln(" <li class=\"dropdown\">");
+document.writeln(" <a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\" role=\"button\" aria-haspopup=\"true\" aria-expanded=\"false\">\
+防守战术 <span class=\"caret\"></span></a>");
+document.writeln("<ul class=\"dropdown-menu\">");
+document.writeln("  <li><a href=\"#\">无</a></li>");
+document.writeln("</ul>");
+document.writeln("</li>");
+document.writeln(" <li class=\"dropdown\">");
+document.writeln(" <a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\" role=\"button\" aria-haspopup=\"true\" aria-expanded=\"false\">\
+残局处理 <span class=\"caret\"></span></a>");
+document.writeln("<ul class=\"dropdown-menu\">");
+document.writeln("  <li><a href=\"baodian.html\">报点要求</a></li>");
+document.writeln("  <li><a href=\"runback.html\">保卫的团队回防</a></li>");
+document.writeln("  <li><a href=\"#\">未更</a></li>");
+document.writeln("  <li role=\"separator\" class=\"divider\"></li>");
+document.writeln("  <li><a href=\"#\">未更</a></li>");
+document.writeln("  <li role=\"separator\" class=\"divider\"></li>");
+document.writeln("  <li><a href=\"#\">未更</a></li>");
+document.writeln("</ul>");
+document.writeln("</li>");
+document.writeln("</ul>");
+document.writeln("</div>");
+document.writeln("</div>");
+document.writeln("</nav>");
+document.writeln("</body>");
+document.writeln("</html>");
+document.writeln("");
